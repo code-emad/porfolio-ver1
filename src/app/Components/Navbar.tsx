964c867ac2063e190a2437dcd306a0e4570ad2e4
Emad <emadul.islam@hotmail.co.uk>
2023-07-23 15:16:14 +0100

    working on navbar and adding styling

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,43 +1,53 @@
-"use client" // this is a client component
-import React from "react"
-import { useState } from "react"
-import { Link } from "react-scroll/modules"
-import { usePathname } from "next/navigation"
-import { useTheme } from "next-themes"
-import { RiMoonFill, RiSunLine } from "react-icons/ri"
-import { IoMdMenu, IoMdClose } from "react-icons/io"
+"use client"; // this is a client component
+import React from "react";
+import { useState } from "react";
+import { Link } from "react-scroll/modules";
+import { usePathname } from "next/navigation";
+import { useTheme } from "next-themes";
+import { RiMoonFill, RiSunLine } from "react-icons/ri";
+import { IoMdMenu, IoMdClose } from "react-icons/io";
 
 interface NavItem {
-    label: string
-    page: string
+  label: string;
+  page: string;
 }
 
 const NAV_ITEMS: Array<NavItem> = [
-    {
-        label: "Home",
-        page: "home",
-    },
-    {
-        label: "About",
-        page: "about",
-    },
-    {
-        label: "Projects",
-        page: "projects",
-    }
-]
-
+  {
+    label: "Home",
+    page: "home",
+  },
+  {
+    label: "About",
+    page: "about",
+  },
+  {
+    label: "Projects",
+    page: "projects",
+  },
+];
 
 const Navbar = () => {
-    const {systemTheme, theme, setTheme} = useTheme()
-    const currentTheme = theme === "system" ? systemTheme : theme
-    const [navbar, setNavbar] = useState(false)
-
-
+  const { systemTheme, theme, setTheme } = useTheme();
+  const currentTheme = theme === "system" ? systemTheme : theme;
+  const [navbar, setNavbar] = useState(false);
 
   return (
-    <div>Navbar</div>
-  )
-}
+    <header>
+      <div className="justify-between md:items-center md:flex">
+        <div>
+          <div>
+            <h2>Emad Islam</h2>
+          </div>
+        </div>
+        <div>
+            {NAV_ITEMS.map((item, idx) => {
+                return <a key={idx}>{item.label}</a>
+            })}
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
