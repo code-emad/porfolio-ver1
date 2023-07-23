3487d58548e3cd9b167afa7417c799654ce14f4e
Emad <emadul.islam@hotmail.co.uk>
2023-07-23 15:02:50 +0100

    installing next themes

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,6 +1,40 @@
-import React from 'react'
+"use client" // this is a client component
+import React from "react"
+import { useState } from "react"
+import { Link } from "react-scroll/modules"
+import { usePathname } from "next/navigation"
+import { useTheme } from "next-themes"
+import { RiMoonFill, RiSunLine } from "react-icons/ri"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
+
+interface NavItem {
+    label: string
+    page: string
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+    {
+        label: "Home",
+        page: "home",
+    },
+    {
+        label: "About",
+        page: "about",
+    },
+    {
+        label: "Projects",
+        page: "projects",
+    }
+]
+
 
 const Navbar = () => {
+    const {systemTheme, theme, setTheme} = useTheme()
+    const currentTheme = theme === "system" ? systemTheme : theme
+    const [navbar, setNavbar] = useState(false)
+
+
+
   return (
     <div>Navbar</div>
   )
