949d812d38700fe5ffba4531e9bc26520e788a39
Emad <emadul.islam@hotmail.co.uk>
2023-07-24 22:05:38 +0100

    adding headshot image

--- src/app/Components/HeroSection.tsx
+++ src/app/Components/HeroSection.tsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Image from 'next/image'
 
 const HeroSection = () => {
   return (
-    <section id="home">HeroSection</section>
+    <section id="home">
+        <div>
+            <div>
+                <Image src="/headshot.png" alt="" width={300} height={300}></Image>
+            </div>
+            <div></div>
+        </div>
+    </section>
   )
 }
 
