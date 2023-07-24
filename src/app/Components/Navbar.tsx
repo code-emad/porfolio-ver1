d1321c566fc0017bafc74d93c24cc8af88bc4d31
Emad <emadul.islam@hotmail.co.uk>
2023-07-24 16:45:15 +0100

    making colour of text black in both light and dark

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -1,15 +1,15 @@
-"use client"; // this is a client component
-import React from "react";
-import { useState } from "react";
-import { Link } from "react-scroll/modules";
-import { usePathname } from "next/navigation";
-import { useTheme } from "next-themes";
-import { RiMoonFill, RiSunLine } from "react-icons/ri";
-import { IoMdMenu, IoMdClose } from "react-icons/io";
+"use client" // this is a client component
+import React from "react"
+import { useState } from "react"
+import { Link } from "react-scroll/modules"
+import { usePathname } from "next/navigation"
+import { useTheme } from "next-themes"
+import { RiMoonFill, RiSunLine } from "react-icons/ri"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
 
 interface NavItem {
-  label: string;
-  page: string;
+  label: string
+  page: string
 }
 
 const NAV_ITEMS: Array<NavItem> = [
@@ -25,20 +25,21 @@ const NAV_ITEMS: Array<NavItem> = [
     label: "Projects",
     page: "projects",
   },
-];
+]
 
 const Navbar = () => {
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === "system" ? systemTheme : theme;
+
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
+        <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
       <div className="justify-between md:items-center md:flex">
         <div>
-          <div className="flex items-center justify-between py-3">
+        <div className="flex items-center justify-between py-3 md:py-5 md:block">
             <div className="md:py-5 md:block">
-              <h2 className="text-2xl font-bold">Emad Islam</h2>
+              <h2 className="text-2xl font-bold text-stone-900">Emad Islam</h2>
             </div>
             <div className="md:hidden">
               <button onClick={()=>setNavbar(!navbar)}>
@@ -50,9 +51,25 @@ const Navbar = () => {
         <div className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
               navbar ? "block" : "hidden"
             }`}>
-        <div className="items-center justify-center md:flex md:space-x-6">
+        <div className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
           {NAV_ITEMS.map((item, idx) => {
-            return <a key={idx}>{item.label}</a>;
+            return (
+              <Link
+                key={idx}
+                to={item.page}
+                className={
+                  "block lg:inline-block text-neutral-900  hover:text-neutral-500 dark:text-neutral-100"
+                }
+                activeClass="active"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+                onClick={() => setNavbar(!navbar)}
+              >
+                {item.label}
+              </Link>
+            )
           })}
           {currentTheme === "dark" ? (
             <button
