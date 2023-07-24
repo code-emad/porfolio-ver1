7e464be6c6b001dde25ede57d522d02049c8d6f2
Emad <emadul.islam@hotmail.co.uk>
2023-07-24 16:01:12 +0100

    installing react icons

--- src/app/Components/Navbar.tsx
+++ src/app/Components/Navbar.tsx
@@ -33,17 +33,26 @@ const Navbar = () => {
   const [navbar, setNavbar] = useState(false);
 
   return (
-    <header>
+    <header className="w-full mx-auto  px-4 sm:px-20 fixed top-0 z-50 shadow bg-white dark:bg-stone-900 dark:border-b dark:border-stone-600">
       <div className="justify-between md:items-center md:flex">
         <div>
-          <div>
-            <h2>Emad Islam</h2>
+          <div className="md:py-5 md:block">
+            <h2 className="text-2xl font-bold">Emad Islam</h2>
           </div>
         </div>
-        <div>
+        <div className="md:flex md:space-x-6">
             {NAV_ITEMS.map((item, idx) => {
                 return <a key={idx}>{item.label}</a>
             })}
+            {currentTheme === "dark" ? (
+              <button>
+                <RiSunLine/>
+              </button>
+            ) : (
+              <button>
+
+              </button>
+            )}
         </div>
       </div>
     </header>
