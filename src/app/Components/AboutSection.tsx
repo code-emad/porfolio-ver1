5af6394512e21e9e4d1c498afde2dd1adb158267
Emad <emadul.islam@hotmail.co.uk>
2023-07-25 16:28:25 +0100

    adding styling and about me image

--- src/app/Components/AboutSection.tsx
+++ src/app/Components/AboutSection.tsx
@@ -1,13 +1,30 @@
 import React from "react";
+import Image from "next/image"
+
+const skills = [
+    { skill: "HTML" },
+    { skill: "CSS" },
+    { skill: "JavaScript" },
+    { skill: "TypeScript" },
+    { skill: "Python" },
+    { skill: "C#" },
+    { skill: "SQL" },
+    { skill: "React" },
+    { skill: "Next.js" },
+    { skill: "Git" },
+    { skill: "GitHub" }, 
+  ]
 
 const AboutSection = () => {
   return (
     <section id="about">
-      <div>
-        <h1>About Me</h1>
-        <div>
-          <div>
-            <h1 className="">Get to know me!</h1>
+      <div className="my-12 pb-12 md:pt-16 md:pb-48">
+        <h1 className="text-center font-bold text-4xl">About Me
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+        <div className="flex flex-col space-y-10 items-stretch justify-center align-top md:space-x-10 md:space-y-0 md:p-4 md:flex-row md:text-left">
+          <div className="md:w-1/2">
+            <h1 className="text-center text-2xl font-bold mb-6 md:text-left">Get to know me!</h1>
             <p>
               Hi, my name is Emad and I am a{" "}
               <span className="font-bold">{"highly ambitious"}</span>,
@@ -38,9 +55,22 @@ const AboutSection = () => {
               always open to new opportunities. 🙂
             </p>
           </div>
-          <div>
-            <h1>My Skills</h1>
-            <p></p>
+          <div className="md:w-1/2 ">
+            <h1 className="text-2xl font-bold mb-6">My Skills</h1>
+            <div className="flex flex-wrap flex-row justify-center z-10 md:justify-start">
+            {skills.map((item, idx) => {
+                return (
+                  <p
+                    key={idx}
+                    className="bg-gray-200 px-4 py-2 mr-2 mt-2 text-gray-500 rounded font-semibold"
+                  >
+                    {item.skill}
+                  </p>
+                )
+              })}
+            </div>
+            <Image className=" hidden md:block md:relative md:bottom-4 md:left-10 md:z-0 " alt="" src="/Animated-Emad.png" width={350} height={350}></Image>
+            
           </div>
         </div>
       </div>
