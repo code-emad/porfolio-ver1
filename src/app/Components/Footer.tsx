61eab9c2d10f6dd0f31527d5e693fdc0d53be12a
Emad <emadul.islam@hotmail.co.uk>
2023-07-26 18:50:48 +0100

    adding social tags

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -1,9 +1,59 @@
-import React from 'react'
+import React from "react";
+import {
+  AiOutlineGithub,
+  AiOutlineTwitter,
+  AiOutlineLinkedin,
+  AiOutlineYoutube,
+} from "react-icons/ai";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+      <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
+      <div>
+        <div>2023 Emad Islam</div>
+        <div>
+        <a href="https://github.com/hqasmei" rel="noreferrer" target="_blank">
+            <AiOutlineGithub
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+          <a
+            href="https://twitter.com/hqasmei"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineTwitter
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
 
-export default Footer
+          <a
+            href="https://www.linkedin.com/in/hosnaqasmei/"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineLinkedin
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+          <a
+            href="https://www.youtube.com/channel/UCQBMkSDgbxDb8usMeXmOZyA"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <AiOutlineYoutube
+              className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
+              size={30}
+            />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
