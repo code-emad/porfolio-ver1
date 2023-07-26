eba186891ed8d0bf99da17a8fee11dbf669e2131
Emad <emadul.islam@hotmail.co.uk>
2023-07-26 17:35:30 +0100

    adding project section to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "./Components/HeroSection"
 import AboutSection from "./Components/AboutSection"
+import ProjectSection from "./Components/ProjectSection"
 
 export default function Home() {
   return (
     <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
       <HeroSection/>
       <AboutSection/>
+      <ProjectSection/>
     </main>
   )
 }
