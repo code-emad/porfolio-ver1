9dc9af3b77557ea906027084600cc8e5fb97a692
Emad <emadul.islam@hotmail.co.uk>
2023-07-26 17:53:47 +0100

    adding title and description for each project

--- src/app/Components/ProjectSection.tsx
+++ src/app/Components/ProjectSection.tsx
@@ -1,61 +1,69 @@
-import React from 'react'
-import Image from "next/image"
-import Link from "next/link"
-import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs";
 
 const projects = [
-    {
-      name: "Project 1",
-      description:
-        "Project 1 is a web app that generates an appreciative sentence of something or someone you are thankful for.",
-      image: "/thankfulthoughts.png",
-      github: "https://github.com/hqasmei/thankful-thoughts",
-      link: "https://thankfulthoughts.io/",
-    },
-    {
-      name: "Project 2",
-      description: "Project 2 is a to do list app that built using the PERN stack.",
-      image: "/platoio.png",
-      github: "https://github.com/hqasmei/platoio",
-      link: "https://platoio.com/register",
-    },
-    {
-      name: "Project 3",
-      description:
-        "Project 3 Photos is a photos and video digitization service in the LA area.",
-      image: "/familyphotos.png",
-      github: "https://github.com/hqasmei/katorfamilyphotos",
-      link: "https://katorfamilyphotos.com/",
-    },
-  ]
+  {
+    name: "Project 1",
+    description:
+      "Project 1 is a web app that generates an appreciative sentence of something or someone you are thankful for.",
+    image: "/thankfulthoughts.png",
+    github: "https://github.com/hqasmei/thankful-thoughts",
+    link: "https://thankfulthoughts.io/",
+  },
+  {
+    name: "Project 2",
+    description:
+      "Project 2 is a to do list app that built using the PERN stack.",
+    image: "/platoio.png",
+    github: "https://github.com/hqasmei/platoio",
+    link: "https://platoio.com/register",
+  },
+  {
+    name: "Project 3",
+    description:
+      "Project 3 Photos is a photos and video digitization service in the LA area.",
+    image: "/familyphotos.png",
+    github: "https://github.com/hqasmei/katorfamilyphotos",
+    link: "https://katorfamilyphotos.com/",
+  },
+];
 
 const ProjectSection = () => {
   return (
     <section id="projects">
-         <h1 className="text-center font-bold text-4xl">Projects
+      <h1 className="text-center font-bold text-4xl">
+        Projects
         <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
-        </h1>
+      </h1>
 
-        <div className='flex flex-col space-y-28'>
-            {projects.map((project, inx) => {
-                return <div key={inx}>
-                    <div className='flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12'>
-                        <div><Image
-                         src={project.image}
-                         alt=""
-                         width={1000}
-                         height={1000}
-                         className="rounded-xl shadow-xl hover:opacity-70"/></div>
-                        <div></div>
-                    </div>
+      <div className="flex flex-col space-y-28">
+        {projects.map((project, inx) => {
+          return (
+            <div key={inx}>
+              <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                <div className="md:w-1/2">
+                  <Image
+                    src={project.image}
+                    alt=""
+                    width={1000}
+                    height={1000}
+                    className="rounded-xl shadow-xl hover:opacity-70"
+                  />
                 </div>
-            })}
-
-
-
-        </div>
+                <div className="md:w-1/2">
+                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">{project.description}</p>
+                    <div></div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default ProjectSection
+export default ProjectSection;
