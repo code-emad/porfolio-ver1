642261acfc9fa2e5922b2fc3525e936b64557e64
Emad <emadul.islam@hotmail.co.uk>
2023-07-26 18:53:53 +0100

    footer first draft complete

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -10,9 +10,9 @@ const Footer = () => {
   return (
     <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
       <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
-      <div>
-        <div>2023 Emad Islam</div>
-        <div>
+      <div className="mx-auto  p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
+        <div className="flex flex-row items-center justify-center space-x-1 text-neutral-500 dark:text-neutral-100">2023 Emad Islam</div>
+        <div className="flex flex-row items-center justify-center space-x-2 mb-1">
         <a href="https://github.com/hqasmei" rel="noreferrer" target="_blank">
             <AiOutlineGithub
               className="hover:-translate-y-1 transition-transform cursor-pointer text-neutral-500 dark:text-neutral-100"
