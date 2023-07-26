ba626c5409938f263fcb6f213023b0c2af6c81fd
Emad <emadul.islam@hotmail.co.uk>
2023-07-26 17:40:11 +0100

    adding mock projects

--- src/app/Components/ProjectSection.tsx
+++ src/app/Components/ProjectSection.tsx
@@ -1,4 +1,33 @@
 import React from 'react'
+import Image from "next/image"
+import Link from "next/link"
+import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
+
+const projects = [
+    {
+      name: "Project 1",
+      description:
+        "Project 1 is a web app that generates an appreciative sentence of something or someone you are thankful for.",
+      image: "/thankfulthoughts.png",
+      github: "https://github.com/hqasmei/thankful-thoughts",
+      link: "https://thankfulthoughts.io/",
+    },
+    {
+      name: "Project 2",
+      description: "Project 2 is a to do list app that built using the PERN stack.",
+      image: "/platoio.png",
+      github: "https://github.com/hqasmei/platoio",
+      link: "https://platoio.com/register",
+    },
+    {
+      name: "Project 3",
+      description:
+        "Project 3 Photos is a photos and video digitization service in the LA area.",
+      image: "/familyphotos.png",
+      github: "https://github.com/hqasmei/katorfamilyphotos",
+      link: "https://katorfamilyphotos.com/",
+    },
+  ]
 
 const ProjectSection = () => {
   return (
