9d0794da5e377fab9f7b35339cd8ed0bb0cdc934
Emad <emadul.islam@hotmail.co.uk>
2023-07-26 17:42:38 +0100

    adding title to section

--- src/app/Components/ProjectSection.tsx
+++ src/app/Components/ProjectSection.tsx
@@ -31,7 +31,12 @@ const projects = [
 
 const ProjectSection = () => {
   return (
-    <div>ProjectSection</div>
+    <section id="projects">
+         <h1 className="text-center font-bold text-4xl">Projects
+        <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+
+    </section>
   )
 }
 
